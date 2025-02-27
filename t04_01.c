3c4f63615ee1caad080bca5421d148c0bff6f987
IndahSiahaan <triyunisiahaan@gmail.com>
2025-02-27 14:37:38 +0700

    Sudah selesai

--- t04_01.c
+++ t04_01.c
@@ -1,10 +1,29 @@
-// NIM - Name
-// NIM - Name
+// 12S24017 - Arya Pratama Sinambela
+// 12S24052 - Indah Triyuni Siahaan
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int _argv, char **_argc) {
-  // codes
+    char input[60];
+    scanf("%s", input);
 
-  return 0;
+    int length = strlen(input);
+    int num_blocks = length / 3;
+    char blocks[num_blocks][4];
+    char hasil[num_blocks + 1];
+
+    int x, z = 0;
+    for (x = 0; x < num_blocks; x++) {
+        strncpy(blocks[x], &input[z], 3);
+        blocks[x][3] = '\0';
+        z += 3;
+        hasil[x] = (char)atoi(blocks[x]);
+    }
+    hasil[num_blocks] = '\0';
+    
+    printf("%s\n", hasil);
+
+    return 0;
 }
--- t04_02.c
+++ t04_02.c
@@ -1,10 +1,20 @@
-// NIM - Name
-// NIM - Name
+// 12S24017 - Arya Pratama Sinambela
+// 12S24052 - Indah Triyuni Siahaan
 
-#include <stdio.h>
-
-int main(int _argv, char **_argc) {
-  // codes
-
-  return 0;
-}
+  #include <stdio.h>
+  #include <stdlib.h>
+  #include <string.h>
+  
+  int main(int _argv, char **_argc) {
+      char input[21];
+      scanf("%20s[^\n]", input);
+  
+      int length = strlen(input);
+      for (int i =0; i < length; i++) {
+      printf ("%03d", input[i]);
+      }
+      
+      printf("013");
+      
+      return 0;
+  }
